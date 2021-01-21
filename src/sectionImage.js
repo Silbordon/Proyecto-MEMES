da64881071e56767809a5ce5d55c0c3e47e05213
Silvina Bordon <silbordon.89@gmail.com>
2021-01-21 19:07:12 -0300

    Se corrije filters de la seccion imagen con una funcion para guardar todos los valores y que no sobreescriba. Se da funcion aÃl boton resstablecer.

--- src/sectionImage.js
+++ src/sectionImage.js
@@ -18,6 +18,8 @@ const hue = document.getElementById('hue'); //Filter hue
 const saturated = document.getElementById('saturated'); //Filter saturated
 const invert = document.getElementById('invert'); //Filter inverted
 
+const resetFilters = document.getElementById('reset-filters'); //Button reset filters
+
 
 
 
@@ -46,51 +48,107 @@ backStyles.addEventListener('change', ()=>{
 
 
 
-// Function Filters
-brightness.addEventListener('input', ()=>{
-    const valueBritness = brightness.value;
-    imageMeme.style.filter = `brightness(${valueBritness})`
-    
-});
 
-opacity.addEventListener('input', ()=>{
-    const valueOpacity = opacity.value;
-    imageMeme.style.filter = `opacity(${valueOpacity})`
-});
-
-contrast.addEventListener('input', ()=>{
-    const valueContrast = contrast.value;
-    imageMeme.style.filter = `contrast(${valueContrast}%)`
-});
+// Function Filters Short
+const actualizarFilters = () =>{
+    imageMeme.style.filter = `brightness(${brightness.value}) opacity(${opacity.value}) contrast(${contrast.value}%) blur(${blurFilter.value}px) grayscale(${grayscale.value}%) sepia(${sepia.value}%) hue-rotate(${hue.value}deg) saturate(${saturated.value}%) invert(${invert.value})`
+};
 
 
-blurFilter.addEventListener('input', ()=>{
-    const valueBlur = blurFilter.value;
-    imageMeme.style.filter = `blur(${valueBlur}px)`
-});
+brightness.addEventListener('input', actualizarFilters);
+opacity.addEventListener('input', actualizarFilters);
+contrast.addEventListener('input', actualizarFilters);
+blurFilter.addEventListener('input', actualizarFilters);
+grayscale.addEventListener('input', actualizarFilters);
+sepia.addEventListener('input', actualizarFilters);
+hue.addEventListener('input', actualizarFilters);
+saturated.addEventListener('input', actualizarFilters);
+invert.addEventListener('input', actualizarFilters);
 
 
-grayscale.addEventListener('input', ()=>{
-    const valueGrayScale = grayscale.value;
-    imageMeme.style.filter = `grayscale(${valueGrayScale}%)`
-});
 
-sepia.addEventListener('input', ()=>{
-    const valueSepia = sepia.value;
-    imageMeme.style.filter = `sepia(${valueSepia}%)`
-});
 
-hue.addEventListener('input', ()=>{
-    const valueHue = hue.value;
-    imageMeme.style.filter = `hue-rotation(${valueHue}deg)`
-});
+// Resolution Filter Longer
+// const actualizarFilters = () =>{
+//     imageMeme.style.filter = `brightness(${brightness.value}) opacity(${opacity.value}) contrast(${contrast.value}%) blur(${blurFilter.value}px) grayscale(${grayscale.value}%) sepia(${sepia.value}%) hue-rotate(${hue.value}deg) saturate(${saturated.value}%) invert(${invert.value})`
+// };
 
-saturated.addEventListener('input', ()=>{
-    const valueSaturated = saturated.value;
-    imageMeme.style.filter = `saturation(${valueSaturated}%)`
-});
 
-invert.addEventListener('input', ()=>{
-    const valueInvert = invert.value;
-    imageMeme.style.filter = `invert(${valueInvert})`
+
+// brightness.addEventListener('input', ()=>{
+//     const valueBritness = brightness.value;
+//     imageMeme.style.filter = `brightness(${valueBritness})`
+//     actualizarFilters();
+// });
+
+
+// opacity.addEventListener('input', ()=>{
+//     const valueOpacity = opacity.value;
+//     imageMeme.style.filter = `opacity(${valueOpacity})`
+//     actualizarFilters();
+// });
+
+
+// contrast.addEventListener('input', ()=>{
+//     const valueContrast = contrast.value;
+//     imageMeme.style.filter = `contrast(${valueContrast}%)`
+//     actualizarFilters();
+// });
+
+
+// blurFilter.addEventListener('input', ()=>{
+//     const valueBlur = blurFilter.value;
+//     imageMeme.style.filter = `blur(${valueBlur}px)`
+//     actualizarFilters();
+// });
+
+
+// grayscale.addEventListener('input', ()=>{
+//     const valueGrayScale = grayscale.value;
+//     imageMeme.style.filter = `grayscale(${valueGrayScale}%)`
+//     actualizarFilters();
+// });
+
+
+// sepia.addEventListener('input', ()=>{
+//     const valueSepia = sepia.value;
+//     console.log(valueSepia);
+//     imageMeme.style.filter = `sepia(${valueSepia}%)`
+//     actualizarFilters();
+// });
+
+
+// hue.addEventListener('input', ()=>{
+//     const valueHue = hue.value;
+//     imageMeme.style.filter = `hue-rotate(${valueHue}deg)`
+//     actualizarFilters();
+// });
+
+// saturated.addEventListener('input', ()=>{
+//     const valueSaturated = saturated.value;
+//     imageMeme.style.filter = `saturate(${valueSaturated}%)`
+//     actualizarFilters();
+// });
+
+// invert.addEventListener('input', ()=>{
+//     const valueInvert = invert.value;
+//     imageMeme.style.filter = `invert(${valueInvert})`
+//     actualizarFilters();
+// });
+
+
+
+// Function Reset Filters
+resetFilters.addEventListener('click', ()=>{
+    brightness.value = '1';
+    opacity.value= '1';
+    contrast.value= '100';
+    blurFilter.value= '0';
+    grayscale.value = '0';
+    sepia.value = '0';
+    hue.value= '0';
+    saturated.value = '100';
+    invert.value = '0';
+    actualizarFilters();
+    // imageMeme.style.filter = '';
 });
