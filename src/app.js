9c7721e947312df8ab4e8b06f5b246cff190caba
Silvina Bordon <silbordon.89@gmail.com>
2021-01-23 16:26:58 -0300

    Se agregan modificaciones en el html para mejorar el responsive, surgen errores con js que seran vistos a continuacion

--- src/app.js
+++ src/app.js
@@ -8,77 +8,108 @@ const sectionMain = document.getElementById('section-main')
 // Image y cancel
 const btnImage = document.getElementById('btn-image');
 const sectionImage = document.getElementById('section-image');
-const btncancelImage = document.getElementById('check-img');
+const btncancel = document.getElementById('cancel');
+const asideContainerGeneral = document.getElementById('aside-container-general');
 
-// Text y cancel
+// Text 
 const btnText = document.getElementById('btn-text')
 const sectionText = document.getElementById('section-text');
-const btncancelText = document.getElementById('cancel-text');
+
 
 // Button Themes
 const btnModeLight = document.getElementById('btn-modeLight');
 const btnModeDark = document.getElementById('btn-modeDark');
 
 
+if(window.innerWidth>700){
+    asideContainerGeneral.classList.remove('none');   
+}
 
 // Function button Image y cancel
 btnImage.addEventListener('click', ()=>{
-    if(window.innerWidth<700){
-        sectionImage.style.display = 'block';
-        sectionHeader.classList.add('none')
-        sectionMain.classList.add('none');
-        btncancelImage.style.display = 'block';
-    }
-    if(window.innerWidth>=700 && window.innerWidth<1300){
-        sectionImage.style.display = 'block'; 
-        sectionText.style.display ='none';  
-    }
-    if(window.innerWidth>=1300){
-        sectionImage.style.display = 'block'; 
-        sectionText.style.display ='none';  
-    }
-})
+    sectionImage.style.display = 'block';
+    sectionText.style.display ='none';  
+
+});
+
+// btncancel.addEventListener('click', ()=>{
+//     alert('funciona')
+//     sectionImage.style.display = 'none'; 
+//     sectionText.style.display = 'none';  
+// });
 
 
-
-btncancelImage.addEventListener('click', ()=>{
-    if(window.innerWidth<1300){
-        sectionImage.style.display = 'none';
-        sectionHeader.classList.remove('none');
-        sectionMain.classList.remove('none');
-    }
+btnText.addEventListener('click', ()=>{
+    sectionText.style.display = 'block';
+    sectionImage.style.display = 'none';
 });
 
 
+// btnImage.addEventListener('click', ()=>{
+//     if(window.innerWidth<700){
+//         sectionImage.style.display = 'block';
+//         sectionHeader.classList.add('none')
+//         sectionMain.classList.add('none');
+//         btncancel.style.display = 'block';
+//         asideContainerGeneral.style.display = 'block'; 
+//     }
+//     if(window.innerWidth>=700 && window.innerWidth<1300){
+//         sectionImage.style.display = 'block'; 
+//         sectionText.style.display ='none'; 
+//         asideContainerGeneral.style.display = 'block';  
+//     }
+//     if(window.innerWidth>=1300){
+//         sectionImage.style.display = 'block';
+//         asideContainerGeneral.style.display = 'block'; 
+//         sectionText.style.display ='none';  
+//     }
+// })
 
 
-// Function button Text y cancel
-btnText.addEventListener('click', ()=>{
-    if(window.innerWidth<700){
-    sectionText.classList.remove('none');
-    sectionText.style.display = 'block';
-    sectionHeader.classList.add('none');
-    sectionMain.classList.add('none');
-    }
-    if(window.innerWidth>=700 && window.innerWidth<1300){
-        sectionText.classList.remove('none'); 
-        sectionText.style.display = 'block';
-        sectionImage.classList.add('none');
-    }
-    if(window.innerWidth>=1300){
-        sectionText.classList.remove('none');
-        sectionImage.classList.add('none');
-        sectionText.style.display = 'block';
+
+// btncancel.addEventListener('click', ()=>{
+//     if(window.innerWidth<1300){
+//         sectionImage.style.display = 'none';
+//         sectionHeader.classList.remove('none');
+//         sectionMain.classList.remove('none');
+//     }
+// });
+
+
+
+
+// // Function button Text y cancel
+// btnText.addEventListener('click', ()=>{
+//     if(window.innerWidth<700){
+//     sectionText.classList.remove('none');
+//     sectionText.style.display = 'block';
+//     sectionHeader.classList.add('none');
+//     sectionMain.classList.add('none');
+//     btncancelText.style.display = 'block';
+//     }
+//     if(window.innerWidth>=700 && window.innerWidth<1300){
+//         sectionText.classList.remove('none'); 
+//         sectionText.style.display = 'block';
+//         sectionImage.classList.add('none');
+//     }
+//     if(window.innerWidth>=1300){
+//         sectionText.classList.remove('none');
+//         sectionImage.classList.add('none');
+//         sectionText.style.display = 'block';
           
-    }
-})
+//     }
+// })
 
-btncancelText.addEventListener('click', ()=>{
-    sectionText.style.display = 'none';
-    sectionText.classList.add('none');
-    sectionHeader.classList.remove('none');
-    sectionMain.classList.remove('none');
-})
+// btncancelText.addEventListener('click', ()=>{
+//     sectionText.style.display = 'none';
+//     sectionText.classList.add('none');
+//     sectionHeader.classList.remove('none');
+//     sectionMain.classList.remove('none');
+// })
+
+
+
+
 
 
 
